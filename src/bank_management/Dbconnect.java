15627b07e6efad56441d94445d3f8ce1acec0ee9
MRizk01 <104647849+MRizk01@users.noreply.github.com>
2023-05-22 17:58:16 +0300

    Initial commit

---
+++ src/bank_management/Dbconnect.java
@@ -0,0 +1,21 @@
+
+package bank_management;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class Dbconnect {
+    static String url = "jdbc:mysql://localhost:3306/";
+    static String user = "root";
+    static String password = "";
+    public static Connection dbconnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url,user,password);
+            return conn;
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return null;
+    }
+}
